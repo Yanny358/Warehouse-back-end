2c4e0bb81927ed10ef255fe13e5169324d74a007
jametl <jametl@ttu.ee>
2022-03-28 13:38:09 +0300

    Ignore Migration

---
+++ WarehouseAPI/Migrations/20220328101416_Ignore.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WarehouseAPI.Migrations
+{
+    public partial class Ignore : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
